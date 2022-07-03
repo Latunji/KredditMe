cb5411e8408b5327d0017bd5bafdc8ddc4fe766f
TSyntax <105598618+TSyntax@users.noreply.github.com>
2022-07-03 14:10:32 +0100

    new update

---
+++ src/main/java/com/kredditme/Models/CrowdFundingCreation.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kredditme.Models;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Lob;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author USER
+ */
+@Data
+@Getter
+@Setter
+public class CrowdFundingCreation {
+    
+    private Long userId;
+    
+    private String headline;
+    
+    private String description;
+    
+    protected byte[] image;
+    
+    private double amount;
+    
+}
